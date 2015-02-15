e97253cf870b43859be9051b4870bdf375b227d6
npmajisha <npmajisha@bitbucket.org>
2015-02-14 22:01:31 -0800

    perceplearn changes

--- perceplearn.py
+++ perceplearn.py
@@ -3,91 +3,118 @@
 #todo -- think of dev file
 import sys
 import re
+import argparse
+import copy
 
-class perceptron:
+class perceptron_train:
     
     def __init__(self):
-        self.labels = {}
+        self.labels = []
+        self.maxIter = 10
+    
+    def write_weights_file(self, feature_weight_vector, modelfile):
+        
+        output = open(modelfile,'w+')
+        for label in sorted(self.labels):
+            output.write(label+" ",)
+            
+        return
         
     
-    def learn(self,inputfile):
-        print("I am learning")
+    def learn(self, trainingfile , devfile):
         
-        #all labels in the training data    
-        label_tags = []
-        #feature vector corresponds to each label
+        training_file = open(trainingfile, "r+")
         feature_vector = {}
+        avg_feature_vector = {}
+        feature_weights = {}
+        avg_feature_weights = {}
+        #preprocessing
+        for line in training_file:
+            tokens = []
             
-        #format of training file--> LABEL feature1 feature2 ... featureN
+            tokens = re.split(r'\s+',line.rstrip())
+            if tokens[0] not in self.labels:
+                self.labels.append(tokens[0])
+                feature_vector[tokens[0]] = {}
+                avg_feature_vector[tokens[0]] = {}
+                
+            for token in tokens[1:]:
+                if token not in feature_weights:
+                    feature_weights[token] = 0
+                    avg_feature_weights[token] = 0
         
+        training_file.close()
+        
+        for label in self.labels:
+            feature_vector[label] = copy.deepcopy(feature_weights)
+            avg_feature_vector[label] = copy.deepcopy(avg_feature_weights)
         
         #iterate n times
-        for i in range(10):
+        c = 1
+        for i in range(self.maxIter):
             print("Iteration :",i)
             #open the training file
-            training_file = open(inputfile,"r+")
+            training_file = open(trainingfile,"r+")
             #iterate through the training data
+            
             for line in training_file:
-                features = []
+                words = []
                 
                 #get all the separate features, split on whitespace
-                features = re.split(r'\s+' , line.rstrip())
-                
-                #check if no labels have been found yet, if yes then assign the same label
-                if len(self.labels) == 0:
-                    #to do -assign the label itself
-                    print("First example")
-                
-                
-                #training data Begins with label    
-                if features[0] not in self.labels:
-                    self.labels[features[0]] = 1
-                
+                words = re.split(r'\s+' , line.rstrip())
+                                             
                 calculated_weights = {}
+                
                 for label in self.labels:  
                     
+                    feature_weights = feature_vector[label]
                     weight_calc = 0
                       
-                    for feature in features[1:]:
-                        
-                        if feature not in feature_vector:
-                            
-                            weight_vectors = {}
-                            for tag in self.labels:
-                                weight_vectors[tag] = 0
-                            feature_vector[feature] = weight_vectors
-                            
-                        else:
-                            #feature is existing in the feature - calculate weight of all the classes
-                            weight_vectors = feature_vector[feature]
-                            if label in weight_vectors:
-                                weight_calc += weight_vectors[label]
-                            else:
-                                weight_vectors[label] = 0
+                    for word in words[1:]:
+                        weight_calc += feature_weights[word]
                     
                     calculated_weights[label] = weight_calc
                 
                 classified_label = sorted(calculated_weights,key = calculated_weights.get, reverse = True)[0]
                 print(classified_label)
-                if features[0] != classified_label:
-                    for feature in features[1:]:
-                        weight_vector = feature_vector[feature]
-                        weight_vector[classified_label] -= 1
-                        weight_vector[features[0]] += 1
-            
-            
+                
+                if words[0] != classified_label:
+                    c_weight_vector = feature_vector[classified_label]
+                    c_avg_weight_vector = avg_feature_vector[classified_label]
+                    a_weight_vector = feature_vector[words[0]]
+                    a_avg_weight_vector = avg_feature_vector[words[0]]
+                    for word in words[1:]:
+                        c_weight_vector[word] -= 1
+                        c_avg_weight_vector[word] -= c*1
+                        a_weight_vector[word] += 1
+                        a_avg_weight_vector[word] += c*1
+                        
+                        
+                c += 1                                
+                
+               
         
-        return
+        return avg_feature_vector
 
 
 def main():
-    #input argument is training filename
+    #parse input arguments
+    parser = argparse.ArgumentParser(add_help = False)
+    parser.add_argument("trainingfile",nargs = "?", help = "trainingfile input filename")
+    parser.add_argument("modelfile",nargs = "?", help = "modelfile output filename")
+    parser.add_argument("-h", nargs = "?", help = "optional devfile for error rate calculation")
+    args = parser.parse_args()
     
     #initialize the perceptron
-    perceptron_c = perceptron()
+    perceptron_c = perceptron_train()
     
     #call the perceptron learn
-    perceptron_c.learn(sys.argv[1])
+    if args.h:
+        avg_f_w_v = perceptron_c.learn(args.trainingfile,args.h)
+    else:
+        avg_f_w_v = perceptron_c.learn(args.trainingfile, 0 )
+        
+    perceptron_c.write_weights_file(avg_f_w_v,args.modelfile)
     
     return
 
