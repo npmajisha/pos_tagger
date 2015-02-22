62bea06cd9d98bf617a5c372edf29f8de15e6b63
npmajisha <npmajisha@bitbucket.org>
2015-02-22 14:00:06 -0800

    touch up

--- perceplearn.py
+++ perceplearn.py
@@ -1,219 +1,224 @@
 #Averaged Perceptron
 #input the training file and output is model file
-#todo -- think of dev file
+#optional devfile
+#todo -- split training file in case devfile is not provided
 import sys
 import re
 import argparse
-import copy
 import pickle
 import percepclassify
+from collections import defaultdict
+import time
 
-def update_avg_weights(f ,d ,avg):
-    for k , v in d.items():
-        avg[k] += f(v)
-        
-def final_avg_weights(iterations, avg):
-    for k, v in avg.items():
-        avg[k] /= iterations
 
+#perceptron training class
 class perceptron_train:
     
     def __init__(self):
+        #List of all the labels that are known
         self.labels = []
+        #fixing the iterations to 30
         self.maxIter = 30
-    
+        #change_counter for each feature - fast approach to averaging weights across iterations
+        self.change_counter = {}
+        #For each label there is a feature_vector , which has weights associated
+        self.feature_vector = {}
+        #Foe each label the avg_feature_vector stores the average weights
+        self.avg_feature_vector = {}
+
+
+    #Below function updates the weights whenever there is a mis-classification
+    #Keeping track of the last weight change of a feature/class combination using
+    #the change_counter dictionary.
+    def update_feature_weights(self,c_iter,label,feature, wt):
+        try:
+            #diff - the number of iterations the value persisted
+            diff = c_iter - self.change_counter[feature][label]
+        except KeyError:
+            diff = c_iter - 1
+            try:
+                change_counter = self.change_counter[feature]
+            except KeyError:
+                self.change_counter[feature] = {}
+                change_counter = self.change_counter[feature]
+            
+        self.change_counter[feature][label] = c_iter
+        #average is calculated using diff
+        self.avg_feature_vector[label][feature] += diff * self.feature_vector[label][feature]
+        #new weight is assigned to the feature
+        self.feature_vector[label][feature] += wt
+
+
+    #only for debugging purpose
+    # def print_feature_weights(self,avg):
+    #     print(self.labels[0])
+    #     print("\n"+"Feature_weights")
+    #     feature_weights =  self.feature_vector[self.labels[0]]
+    #     feature_string = ""
+    #     for feature in sorted(feature_weights):
+    #         feature_string += feature + " "+str(feature_weights[feature])+" "
+    #     print(feature_string)
+    #     if(avg):
+    #         print("Avg_Feature_weights")
+    #         feature_weights =  self.avg_feature_vector[self.labels[0]]
+    #         feature_string = ""
+    #         for feature in sorted(feature_weights):
+    #             feature_string += feature + " "+str(feature_weights[feature])+" "
+    #         print(feature_string)
+                
+        
+    #Writes the learnt model - the average weight vector for the features to the modelfile
     def write_weights_file(self, feature_weight_vector, modelfile):
         
-        output = open(modelfile,'wb')
-        pickle.dump(feature_weight_vector,output,2)
+        output = open(modelfile, "w+")
+
+        pickle.dump(feature_weight_vector,output)
         
         output.close()
-##        op = open("weights.txt", 'w+')
-##        for label in sorted(self.labels):
-##            op.write(label + "\n")
-##            for feature in sorted(feature_weight_vector[label]):
-##                op.write(feature +" "+ str(feature_weight_vector[label][feature]))
-##                op.write("\n")
-
-
-            
         return
         
-    
-    def learn(self, trainingfile , devfile):
-        
-##        output = open("log.txt",'w+')
-        classifier = percepclassify.perceptron_classify()
+    #this is the learn module
+    def learn(self, trainingfile , modelfile,devfile):
+
+        #open the training file
         training_file = open(trainingfile, "r+")
-        feature_vector = {}
-        avg_feature_vector = {}
-        change_counter = {}
-        feature_weights = {}
-        avg_feature_weights = {}
+
+        
+        classifier = percepclassify.perceptron_classify()
+
+        
+        feature_weights = defaultdict(int)
+        avg_feature_weights = defaultdict(int)
+        best_avg_vector = defaultdict(int)
+        
+        prev_error = 1.0
         #preprocessing
+        lines = []
+        i=0
+        
+        
         for line in training_file:
+            
             tokens = []
             
             tokens = re.split(r'\s+',line.rstrip())
+            lines.append(tokens)
+            i += 1
+                
             if tokens[0] not in self.labels:
                 self.labels.append(tokens[0])
-                feature_vector[tokens[0]] = {}
-                avg_feature_vector[tokens[0]] = {}
-                change_counter[tokens[0]] = 1
+                self.feature_vector[tokens[0]] = {}
+                self.avg_feature_vector[tokens[0]] = {}
+                
                 
             for token in tokens[1:]:
                 if token not in feature_weights:
                     feature_weights[token] = 0.0
                     avg_feature_weights[token] = 0.0
+                                                
         
         training_file.close()
         
         for label in self.labels:
-            feature_vector[label] = copy.deepcopy(feature_weights)
-            avg_feature_vector[label] = copy.deepcopy(avg_feature_weights)
+            self.feature_vector[label] = dict(feature_weights)
+            self.avg_feature_vector[label] = dict(avg_feature_weights)
         
         #iterate n times
         c = 1
         for i in range(self.maxIter):
-            print("Iteration :",i)
+            iter_start = time.time()
+            print("Iteration :",i )
             #open the training file
-            training_file = open(trainingfile,"r+")
+            #training_file = open(trainingfile,"r+")
             #iterate through the training data
             
-            for line in training_file:
-                words = []
+            #for line in training_file:
+            
+            for line in lines:
+                print("C:",c)
+                
+                #print("Line:",l)
                 
                 #get all the separate features, split on whitespace
-                words = re.split(r'\s+' , line.rstrip())
+                #words = re.split(r'\s+' , line.rstrip())
+                #words = line
                                              
                 calculated_weights = {}
-                
                 for label in self.labels:  
                     
-                    feature_weights = feature_vector[label]
+                    feature_weights = self.feature_vector[label]
                     weight_calc = 0
                       
-                    for word in words[1:]:
-                        weight_calc += feature_weights[word]
+##                    for word in words[1:]:
+##                        weight_calc += feature_weights[word]
+                        
+                    weight_calc = sum([feature_weights[word] for word in line[1:]])
                     
                     calculated_weights[label] = weight_calc
                 
                 classified_label = sorted(calculated_weights,key = calculated_weights.get, reverse = True)[0]
-                #print(classified_label)
-                
-                #print feature weights and average weights
-##                output.write("\nIteration :"+str(i+1) +" C iteration : " +str(c)+ '\n')
-##                output.write("\n Start of update\n")
-##                for label in self.labels:
-##                    
-##                    output.write("\n"+label + "   Change_counter : " + str(change_counter[label]))
-##                    output.write('\n Feature Vector \n')
-##                    for key in sorted(feature_vector[label]):
-##                        output.write(key + ' ')
-##                    output.write("\n")
-##                    for key in sorted(feature_vector[label]):
-##                        output.write(str(feature_vector[label][key])+ ' ')
-##                    output.write('\n Average feature vector \n')
-##                    for key in sorted(avg_feature_vector[label]):
-##                        output.write(key + ' ')
-##                    output.write("\n")
-##                    for key in sorted(avg_feature_vector[label]):
-##                        output.write(str(avg_feature_vector[label][key])+' ')
-                
-                if words[0] != classified_label:
+
+                actual_label = line[0]
+                if actual_label != classified_label:                                   
                     
-                    c_weight_vector = feature_vector[classified_label]
-                    c_avg_weight_vector = avg_feature_vector[classified_label]
-                    
-                    update_avg_weights(lambda x : x * (c - change_counter[classified_label]),c_weight_vector, c_avg_weight_vector)
-                    
-                    change_counter[classified_label] =  c 
-                                        
-                    a_weight_vector = feature_vector[words[0]]
-                    a_avg_weight_vector = avg_feature_vector[words[0]]                    
-                    
-                    update_avg_weights(lambda x : x * (c - change_counter[words[0]]), a_weight_vector, a_avg_weight_vector)
-                        
-                    change_counter[words[0]] = c 
-                    
-                    for word in words[1:]:
-                        c_weight_vector[word] -= 1
-                        a_weight_vector[word] += 1
-                        
-                    #print feature weights and average weights
-##                    output.write("\n Vector changed\n")
-##                    for label in self.labels:
-##                        
-##                        output.write("\n"+label+ "   Change_counter : " + str(change_counter[label]))
-##                        output.write('\n Feature Vector \n')
-##                        for key in sorted(feature_vector[label]):
-##                            output.write(key + ' ')
-##                        output.write("\n")
-##                        for key in sorted(feature_vector[label]):
-##                            output.write(str(feature_vector[label][key])+ ' ')
-##                        output.write('\n Average feature vector \n')
-##                        for key in sorted(avg_feature_vector[label]):
-##                            output.write(key + ' ')
-##                        output.write("\n")
-##                        for key in sorted(avg_feature_vector[label]):
-##                            output.write(str(avg_feature_vector[label][key])+' ')
-                        
-                        
+                    for word in line[1:]:
+                        self.update_feature_weights(c,classified_label,word,-1.0)
+                        self.update_feature_weights(c,actual_label,word,1.0)
+                    dict_up_end = time.time()
+                    #print("Dictionary", dict_up_end-dict_up_start)
+                self.print_feature_weights(1)
                 c += 1                                
+            iter_end = time.time()
+            #print("Iteration time: ",iter_end-iter_start)
+
                 
-                
-            training_file.close()
+            #training_file.close()
             
             #check against the dev set
             if (devfile):
-                print(str(classifier.check_dev_error(devfile, c_avg_weight_vector)))
+                for label in self.labels:    
+                    for feature in self.feature_vector[label]:
+                        self.update_feature_weights(c-1, label,feature,0)
                 
+                dev_error = classifier.check_dev_error(devfile, self.avg_feature_vector)
+                print("Dev error:",dev_error)
+                if prev_error > dev_error:
+                    prev_error = dev_error
+                    best_avg_vector = dict(self.avg_feature_vector)
+                    
         
+        if (devfile):
+            self.write_weights_file(best_avg_vector,modelfile)        
+            return           
+                 
         for label in self.labels:    
-            c_weight_vector = feature_vector[label]
-            c_avg_weight_vector = avg_feature_vector[label]
-                        
-            
-            update_avg_weights(lambda x : x * (c - change_counter[label]),c_weight_vector, c_avg_weight_vector)
-            final_avg_weights(c - 1,c_avg_weight_vector)
-##            output.write("\n Final weights\n") 
-##            
-##            output.write("\n"+label)
-####            output.write('\n Feature Vector \n')
-####            for key in sorted(feature_vector[label]):
-####                output.write(key + ' ')
-####            output.write("\n")
-####            for key in sorted(feature_vector[label]):
-####                output.write(str(feature_vector[label][key])+ ' ')
-##            output.write('\n Average feature vector \n')
-##            for key in sorted(avg_feature_vector[label]):
-##                output.write(key + ' ')
-##            output.write("\n")
-##            for key in sorted(avg_feature_vector[label]):
-##                output.write(str(avg_feature_vector[label][key])+' ')   
-        
-        return avg_feature_vector
+            for feature in self.feature_vector[label]:
+                self.update_feature_weights(c, label,feature,0)
+     
+        self.print_feature_weights(1)        
+        self.write_weights_file(self.avg_feature_vector,modelfile)        
+        return
 
 
       
 def main():
-    #parse input arguments
+    #parse input arguments  
     parser = argparse.ArgumentParser(add_help = False)
-    parser.add_argument("trainingfile",nargs = "?", help = "trainingfile input filename")
-    parser.add_argument("modelfile",nargs = "?", help = "modelfile output filename")
-    parser.add_argument("-h", nargs = "?", help = "optional devfile for error rate calculation")
+    parser.add_argument("trainingfile",nargs = 1, help = "trainingfile input filename")
+    parser.add_argument("modelfile",nargs = 1, help = "modelfile output filename")
+    parser.add_argument("-h","--DEVFILE",nargs = "?", help = "optional devfile for error rate calculation")
     args = parser.parse_args()
     
     #initialize the perceptron
     perceptron_c = perceptron_train()
     
     #call the perceptron learn
-    if args.h:
-        avg_f_w_v = perceptron_c.learn(args.trainingfile,args.h)
+    if args.DEVFILE:
+        perceptron_c.learn(args.trainingfile[0],args.modelfile[0],args.DEVFILE)
     else:
-        avg_f_w_v = perceptron_c.learn(args.trainingfile, 0 )
-        
-    perceptron_c.write_weights_file(avg_f_w_v,args.modelfile)
+        perceptron_c.learn(args.trainingfile[0],args.modelfile[0], 0 )
+     
+
     
     return  
     
--- postrain.py
+++ postrain.py
@@ -1,22 +1,60 @@
 #POS tag train
 
 import argparse
-import perceplearn
+import opt_perceplearn
 import sys
+import re
 
 def main():
     
     parser = argparse.ArgumentParser(add_help = False)
-    parser.add_argument("trainingfile",nargs = "+", help = "trainingfile input filename")
-    parser.add_argument("modelfile",nargs = "+", help = "modelfile output filename")
-    parser.add_argument("-h", nargs = "?", help = "optional devfile for error rate calculation")
+    parser.add_argument("trainingfile",nargs = 1, help = "trainingfile input filename")
+    parser.add_argument("modelfile",nargs = 1, help = "modelfile output filename")
+    parser.add_argument("-h","--DEVFILE", nargs = "?", help = "optional devfile for error rate calculation")
     args = parser.parse_args()
-    print(args)
+    
     
     #open the training_file and convert to features
-    #save it to a file and 
+    #save it to a file and then pass it to the perceptron
     
-
-
+    pos_train = open(args.trainingfile[0], 'r+')
+    pos_feature = open("pos.train.fmt",'w+')
+    for line in pos_train:
+        comb_word_tag = re.split(r'\s+',line.rstrip())
+        
+        i=0
+        #first token processing
+        word_tag = re.split(r'/',comb_word_tag[0])
+        if len(comb_word_tag) > 1:
+            next_tag = re.split(r'/',comb_word_tag[1])
+        else:
+            next_tag[0] = "E_O_S"
+        feature = word_tag[1]+" "+word_tag[0]+ " w_prev:B_O_S"+" w_next:"+next_tag[0]
+        pos_feature.write(feature+"\n")
+        for token in comb_word_tag[1:-1]:
+            feature = ""
+            i += 1
+            word_tag = re.split(r'/',token)
+            prev_tag = re.split(r'/',comb_word_tag[i-1])
+            next_tag = re.split(r'/',comb_word_tag[i+1])
+            
+            feature += word_tag[1]+" "+word_tag[0]+ " w_prev:"+prev_tag[0]+" w_next:"+next_tag[0]
+            pos_feature.write(feature+"\n")
+        
+        #last token processing
+        if len(comb_word_tag) > 1:
+            prev_tag = re.split(r'/',comb_word_tag[-2])
+            word_tag = re.split(r'/',comb_word_tag[-1])
+            feature = word_tag[1]+" "+word_tag[0]+" w_prev:"+prev_tag[0]+" w_next:E_O_S"
+            pos_feature.write(feature+"\n")
+    
+    pos_train.close()
+    pos_feature.close()
+    
+    #train the perceptron using this formatted file
+    
+    perceptron = opt_perceplearn.perceptron_train()
+    perceptron.learn("pos.train.fmt",args.modelfile[0],0)
+    
 if __name__ == '__main__':
     main()
